f2c650fd26636f13463cf3ba1f511599ee90a21e
Cerberi (Puting Lobo Studios) <reynardashvites@gmail.com>
2025-03-28 17:21:51 +0800

    Update NoStudents.jsx

--- src/components/NoStudents.jsx
+++ src/components/NoStudents.jsx
@@ -4,7 +4,7 @@ const NoStudents = () => (
 
 	<IonRow className="ion-text-center ion-justify-content-center">
     <IonCol size="9">
-      <IonText class='brand-text' color='primary' style={{ fontSize: '2em' }}>Lourdes College</IonText>
+      <IonText class='brand-text' color='primary' style={{ fontSize: '2em' }}>Lourdes College QR Attendance Checker</IonText>
       <img src="/assets/icon2.png" alt="icon" />
       <p>Welcome! <IonText color="primary"> </IonText>. Please click the button in the bottom right to get started.</p>
     </IonCol>
