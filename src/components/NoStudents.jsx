431adc5580e6d9c730f728208a530f6f0d3b2432
Cerberi (Puting Lobo Studios) <reynardashvites@gmail.com>
2025-03-28 17:21:17 +0800

    Update NoStudents.jsx

--- src/components/NoStudents.jsx
+++ src/components/NoStudents.jsx
@@ -6,7 +6,7 @@ const NoStudents = () => (
     <IonCol size="9">
       <IonText class='brand-text' color='primary' style={{ fontSize: '2em' }}>Lourdes College</IonText>
       <img src="/assets/icon2.png" alt="icon" />
-      <p>Welcome to<IonText color="primary"> QR Code Attendance System</IonText>. Please click the button in the bottom right to get started.</p>
+      <p>Welcome! <IonText color="primary"> </IonText>. Please click the button in the bottom right to get started.</p>
     </IonCol>
   </IonRow>
 );
